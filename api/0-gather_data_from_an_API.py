4debb24e3b97e9059ed489bca3449a82814cd678
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-04-25 20:21:30 +0400

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,15 +1,24 @@
-#!usr/bin/python3
-from flask import Flask
-from flask import request
+#!/usr/bin/python3
 import json
-
-app = Flask("__name__")
-
-@app.route("/")
-def response():
-    response = request.get("https://jsonplaceholder.typicode.com/todos")
-
-    print(response)
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}").json().get('name')
+    total_list = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}").json()
+
+    sum_of_list = len(total_list)
+    number_of_done_tasks = 0
+    completed_task_name = ""
+
+    for i in total_list:
+        if i["completed"]:
+            number_of_done_tasks += 1
+            completed_task_name += "\t" + i.get("title") + "\n"
+
+    print(f"Employee {user} is done with tasks({number_of_done_tasks}/{sum_of_list})")
+    print(completed_task_name)
+
+    
+
